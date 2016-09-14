734e9716a8a4b50f55b6f946648606723c0d93bc
shy_azusa <shy.azusa@gmail.com>
2016-09-14 11:56:42 +0900

    cosmetic change (#3)
    * cosmetic change
    
    * cosme

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-window.addEventListener('load', function(eve) {
+window.addEventListener('load', function() {
   document.getElementById('hhmm').addEventListener('click', function() {
-    var hhmm = document.getElementById("hhmm_form").value;
-    var matches = hhmm.match(/([0-9]{2}):([0-9]{2})/);
-    var sec = matches[1] * 60 * 60 + matches[2] *60;
-    document.getElementById("s_form").value = sec;
+    const hhmm = document.getElementById('hhmm_form').value;
+    const matches = hhmm.match(/([0-9]{2}):([0-9]{2})/);
+    const sec = matches[1] * 60 * 60 + matches[2] * 60;
+    document.getElementById('s_form').value = sec;
   });
   document.getElementById('s').addEventListener('click', function() {
-    var sec = document.getElementById("s_form").value;
-    var h = ""+(sec/36000|0)+(sec/3600%10|0)
-    var m = ""+(sec%3600/600|0)+(sec%3600/60%10|0)
-    var s = ""+(sec%60/10|0)+(sec%60%10)
-    var time = h+":"+m;
-    document.getElementById("hhmm_form").value = time;
+    const sec = document.getElementById('s_form').value;
+    const h = '' + (sec / 36000 | 0) + (sec / 3600 % 10 | 0);
+    const m = '' + (sec % 3600 / 600 | 0) + (sec % 3600 / 60 % 10 | 0);
+    const time = h + ':' + m;
+    document.getElementById('hhmm_form').value = time;
   });
 }, false);
